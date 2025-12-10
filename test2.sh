7ffdfcb6e81dff1693024987d160432a6f02844a
Student 008 pp25 <pp25s008@apollo-gpu.lsalab.cs.nthu.edu.tw>
2025-12-10 22:06:22 +0800

    fix bug

--- test2.sh
+++ test2.sh
@@ -1,4 +1,5 @@
 make
+mkdir -p ./.middle
 ./seq ./testcases/testcase2.bin 0.01 300
 ./bin2csv ./.middle/trajectory.bin ./.middle/output.csv 70 300
 python ./Generation/mp4_generator/mp4.py ./.middle/output.csv --n 70 --d 300 --t 10 --output ./Videos/t2.mp4 
--- test3.sh
+++ test3.sh
@@ -1,4 +1,5 @@
 make
+mkdir -p ./.middle
 ./seq ./testcases/testcase3.bin 0.01 300
 ./bin2csv ./.middle/trajectory.bin ./.middle/output.csv 10000 300
 python ./Generation/mp4_generator/mp4.py ./.middle/output.csv --n 10000 --d 300 --t 10 --output ./Videos/t3.mp4 
